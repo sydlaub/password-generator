70ae53a06f395f8f094bb7e5bb2e33a4d460441e
Sydney Laub <syd.laub18@gmail.com>
2023-04-18 16:10:23 -0400

    slight changes to clean up JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// GIVEN I need a new, secure password
-// !WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// ?WHEN prompted for the length of the password
-// ?THEN I choose a length of at least 8 characters and no more than 128 characters
-// ?WHEN asked for character types to include in the password
-// ?THEN I confirm whether or not to include lowercase, uppercase, numeric, and / or special characters
-// ?WHEN I answer each prompt
-// ?THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
 //Password object to store password as its being built
 var passwordCriteria = {
 
@@ -38,38 +21,8 @@ var passwordCriteria = {
 
 }
 
+// variable to store all possible password characters based on user input
 var masterArray = [];
-
-// !Need a variable to store the password as it's being built!
-// Need a variable for the mega-array of characters
-// !Need to ask the user how long the password should be
-// !prompt ()
-// !^ this needs to be stored in a variable
-// Confirm whether to use uppercase letters, lowercase letters, numbers, and/or special characters
-// Need to check whether the user selected a valid password length 
-// need to check whether user selected at least one character type 
-// If not, prompt them again OR alert that the input wasn't valid and start over (If we use multiple functions it should be easy to prompt again, if not we use the return function)
-//Need to make sure that at least one character is chosen from each character type
-// when the user selects a character type, generate a random character from that character set and concatenate it to the unfinished password
-// generate a random character: Math.floor(Math.random() * array.length) gives a RANDOM INDEX, NOT A RANDOM CHARACTER ITSELF
-//Can use .charAt()
-// OR array[Math.floor(Math.random() * array.length)]
-// once we have the actual character, concatenate it to the variable thats storing our unfinished password
-//push that character set to the meg-array
-//once we have our mega-array:
-// for loop:
-// generate a random character: Math.floor(Math.random() * array.length) gives a RANDOM INDEX, NOT A RANDOM CHARACTER ITSELF
-//Can use .charAt()
-// OR array[Math.floor(Math.random() * array.length)]
-// once we have the actual character, concatenate it to the variable thats storing our unfinished password
-// start at i = unfinishedpassword.length
-// go as long as i < passwordLength 
-// return unfinishedPassword
-
-
-
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -127,10 +80,11 @@ function generatePassword() {
     masterArray = masterArray.concat(passwordCriteria.passwordCharacters)
   }
 
-  //create for loop to randomly pick characters from the master array and push them to the newPassword
+  //create for loop to randomly pick characters from the master array and push them into the newPassword
   for(let i = 0; i < passwordCriteria.passLength; i++) {
+    //write code to randomly get character from masterArray
     randomChar = masterArray[Math.floor(Math.random() * masterArray.length)]
-    // after some code to randomly get a character from masterArray, add to newPassword
+    // add character to newPasswod
     newPassword += randomChar
   }
 
